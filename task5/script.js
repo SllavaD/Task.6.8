ca99d082f875479caf89aef63f8bd9c99813a5fc
SllavaD <116106353+SllavaD@users.noreply.github.com>
2022-12-10 14:10:21 +0300

    correct code

--- task5/script.js
+++ task5/script.js
@@ -1,2 +1,18 @@
-const textInput1 = document.querySelector('#textInput');
-console.log(textInput1);
+const inputForm = document.querySelector('#inputForm');
+const text = document.querySelector('#duplicateField');
+const buttonUser = document.querySelector('#buttonUser');
+
+inputForm.addEventListener('input',
+    (event) => {
+        text.textContent = inputForm.value
+        ;
+    });
+
+buttonUser.addEventListener('click',
+    (event) => {
+        event.preventDefault()
+        console.log(text.textContent)
+        inputForm.value = ""
+        text.textContent = ""
+        ;
+    })
